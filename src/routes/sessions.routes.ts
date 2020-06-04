5132ad481219dc81e40ebf9ce81f2336282c3c1f
Djamilson <djamilson@gmail.com>
2020-06-04 01:39:44 -0300

    Criando user

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -4,21 +4,17 @@ import AuthenticationUserService from '../services/AuthenticationUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const authenticanteUser = new AuthenticationUserService();
-    await authenticanteUser.execute({ email, password });
+  const { email, password } = req.body;
+  const authenticanteUser = new AuthenticationUserService();
+  await authenticanteUser.execute({ email, password });
 
-    const { user, token } = await authenticanteUser.execute({
-      email,
-      password,
-    });
-    delete user.password;
+  const { user, token } = await authenticanteUser.execute({
+    email,
+    password,
+  });
+  delete user.password;
 
-    return res.json({ user, token });
-  } catch (error) {
-    return res.status(error.statusCode).json({ error: error.message });
-  }
+  return res.json({ user, token });
 });
 
 export default sessionsRouter;
