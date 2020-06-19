68e68f5049a08448562cecd463dc2b0e24e8ea47
Djamilson <djamilson@gmail.com>
2020-06-19 14:49:54 -0300

    Recuperação de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,43 +2,38 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
-import User from '../infra/typeorm/entities/User';
-import IHashProvider from '../providers/HashProvider/models/IHashProvider';
-import IUsersRepository from '../repositories/IUsersRepository';
+//import User from '../infra/typeorm/entities/User';
+import IUserTokensRepository from '../repositories/IUserTokensRepository'
 
+import IUsersRepository from '../repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
 interface IRequest {
-  name: string;
   email: string;
-  password: string;
 }
 
 @injectable()
-class CreateUserService {
+class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('HashProvider')
-    private hashProvider: IHashProvider,
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+
   ) {}
 
-  public async execute({ name, email, password }: IRequest): Promise<User> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
-
-    if (checkUserExists) {
-      throw new AppError('Email address already used.');
+  public async execute({email}: IRequest): Promise<void> {
+    const userExists = await this.usersRepository.findByEmail(email);
+    if (!userExists){
+      throw new AppError('User does not exists.');
     }
+    await this.userTokensRepository.generate(userExists.id);
 
-    const hashedPassword = await this.hashProvider.generateHash(password);
-
-    const user = this.usersRepository.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    return user;
+    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido')
   }
 }
 
-export default CreateUserService;
+export default SendForgotPasswordEmailService;
